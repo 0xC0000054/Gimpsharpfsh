98a07b5e8587c4d605284ef449536768af1e77a9
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-06-01 03:53:38 +0000

    Initial Import

---
+++ Gimpsharpfsh/ExtensionAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    // a little hack to force extension methods to work under .NET 2.0
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
+    internal sealed class ExtensionAttribute : Attribute
+    {
+    }
+}
