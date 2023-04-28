6dfece290565ca7300ffa36be36ce25b6be51163
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-04-28 00:06:07 -0600

    Prepare the project for public release
    Added README.md and License.txt.
    Added a license header to all existing files.

--- Gimpsharpfsh/ExtensionAttribute.cs
+++ Gimpsharpfsh/ExtensionAttribute.cs
@@ -1,4 +1,25 @@
-﻿namespace System.Runtime.CompilerServices
+﻿/*
+*  This file is part of Gimpsharpfsh, a file format plug-in for GIMP
+*  that loads and saves FSH images.
+*
+*  Copyright (C) 2009, 2010, 2011, 2012, 2023 Nicholas Hayes
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+namespace System.Runtime.CompilerServices
 {
     // a little hack to force extension methods to work under .NET 2.0
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
---
+++ README.md
@@ -0,0 +1,10 @@
+# Gimpsharpfsh
+
+A file format plug-in for GIMP that loads and saves FSH images.
+
+This program depends on [gimp-sharp](https://github.com/mrijk/gimp-sharp) and [FSHLib](https://github.com/0xC0000054/FSHLib).
+
+# License
+
+This project is licensed under the terms of the GNU General Public License version 3.0.   
+See [License.txt](License.txt) for more information.
